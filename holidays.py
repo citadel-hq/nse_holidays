091acd1da938a444d0dbb7103b1779b5b06beda3
Shikhar Vaish <shikarvaish@gmail.com>
2024-04-01 16:13:32 +0530

    fix: pick the correct holidays

--- holidays.py
+++ holidays.py
@@ -2,11 +2,12 @@ import nsepython
 
 
 def main():
-    holidays = nsepython.nse_holidays()
-    holidays = holidays['CBM']
-    for holiday in holidays:
-        with open('nse_holidays.csv', 'a') as f:
-            f.write(f"{holiday['tradingDate']}\n")
+    holidays = nsepython.nse_holidays(type="trading")
+    holidays = holidays['FO']
+    holiday_dates = [holiday['tradingDate'] for holiday in holidays]
+
+    with open('nse_holidays.csv', 'w') as f:
+        f.write("\n".join(holiday_dates))
 
 
 if __name__ == '__main__':
